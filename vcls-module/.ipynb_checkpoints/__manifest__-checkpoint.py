d28a7c5d6c456b51ccabe3e07f26683bf271891d
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2018-12-07 10:03:29 +0000

    helpdesk module version increment

--- vcls-module/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ vcls-module/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -16,15 +16,15 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '0.2.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base',
                 'contacts',
+                'helpdesk',
                 'hr',
                 'hr_contract',
                 'hr_holidays',
-                'helpdesk',
                 'snailmail',
                 'web_studio',
                ],
