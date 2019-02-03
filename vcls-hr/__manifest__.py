252087d13fe0805f3a3c837c78a972bf399432b1
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-02-03 22:14:30 +0000

    increment

--- vcls-hr/__manifest__.py
+++ vcls-hr/__manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.10.3',
+    'version': '0.10.4',
 
     # any module necessary for this one to work correctly
     'depends': ['base',
@@ -45,8 +45,8 @@
         #########
         # VIEWS #
         #########
-        #'views/employee.xml',
-        'views/employee_security_test.xml',
+        'views/employee.xml',
+        #'views/employee_security_test.xml',
         'views/bonuses.xml',
         'views/benefits.xml',
         'views/company_org.xml',
