1896925bb1cb13cf5bb24070024bb246a1e3b5d8
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-02-20 11:17:43 +0000

    activity notification template modified

--- vcls-hr/models/mail_activity.py
+++ vcls-hr/models/mail_activity.py
@@ -17,7 +17,7 @@ _logger = logging.getLogger(__name__)
 
 class MailActivityType(models.Model):
     """ Overrides the initial class """
-    _inherit = 'mail.activity.type'
+    _inherit = 'mail.activity'
     
     
     ''' We override the below method in order to have a specific body template for the leave approval case'''
@@ -25,22 +25,24 @@ class MailActivityType(models.Model):
     def action_notify(self):
         body_template = self.env.ref('mail.message_activity_assigned')
         for activity in self:
+            subject=_('%s: %s assigned to you') % (activity.res_name, activity.summary or activity.activity_type_id.name),
             # if this is a leave approval activity_type
-            raise ValidationError('{} = {}'.format(activity.activity_type_id.id,self.env.ref('hr_holidays.mail_act_leave_approval').id))
             if activity.activity_type_id.id == self.env.ref('hr_holidays.mail_act_leave_approval').id:
                 body_template = self.env.ref('vcls-hr.message_activity_leave')
+                subject=_('Kalpa | Leave Request from {}'.format(activity.create_user_id.name)) 
                 
-            
+                
             model_description = self.env['ir.model']._get(activity.res_model).display_name
             body = body_template.render(
                 dict(activity=activity, model_description=model_description),
                 engine='ir.qweb',
                 minimal_qcontext=True
             )
+            
             self.env['mail.thread'].message_notify(
                 partner_ids=activity.user_id.partner_id.ids,
                 body=body,
-                subject=_('%s: %s assigned to you') % (activity.res_name, activity.summary or activity.activity_type_id.name),
+                subject=subject,
                 record_name=activity.res_name,
                 model_description=model_description,
                 notif_layout='mail.mail_notification_light'
