06038f5de75ca6114687597296990e0bc2185181
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-03-06 10:50:12 +0000

    test trad

--- vcls-hr/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ vcls-hr/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.10.25',
+    'version': '0.10.26',
 
     # any module necessary for this one to work correctly
     'depends': ['base',
