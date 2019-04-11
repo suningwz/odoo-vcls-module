41c130c67df4fa4091e39018b2180bf20826323c
MarwanLaam <laamouri@voisinconsulting.com>
2019-04-11 16:34:01 +0200

    Update change_request.py

--- vcls-helpdesk/models/change_request.py
+++ vcls-helpdesk/models/change_request.py
@@ -9,13 +9,18 @@ class ChangeRequest(models.Model):
     _inherit = 'helpdesk.ticket'
     _name = 'helpdesk.change.request'
     reason_for_change = fields.Text('Reason for Change')
-    due_date = fields.Date()
+    due_date = fields.Date(string="Due Date")
     impact = fields.Integer('10','30','50','70')
-    severity = fields.Selection([('Low','Informational'),('Normal','Minor'),('Significant','Critical')])
-    main_risks = fields.Text()
-    priority = fields.Selection()
-    cmb_meeting_date = fields.Date()
-    validation_status = fields.Selection()
+    severity = fields.Selection(string="Severity",[('1','Low / Informational'),('2','Normal / Minor'),('3','Significant / Critical')])
+    main_risks = fields.Text('Main risks')
+    priority = fields.Selection(string="Priority",[('1','Standard'),('2','Minor'),('3','Medium'),('4','Critical')])
+    cmb_meeting_date = fields.Date(string="CMB Meeting date")
+    validation_status = fields.Selection(string="Validation Status",[('1','Approved'),('2','Rejected'),('3','On Hold')])
+
+    risk_analysis = fields.Many2one() 
+    implementation_testing_plan = fields.Many2one()
+    rollback_plan = fields.Many2one()
+    backout_plan = fields.Many2one()
 
 
 
