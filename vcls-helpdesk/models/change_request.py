73699e9e0dde47b810b6bb6ed2e6150abe804ed8
MarwanLaam <laamouri@voisinconsulting.com>
2019-04-12 09:27:20 +0200

    Update change_request.py

--- vcls-helpdesk/models/change_request.py
+++ vcls-helpdesk/models/change_request.py
@@ -6,7 +6,7 @@ from odoo.exceptions import UserError, ValidationError
 
 class ChangeRequest(models.Model):
     
-    _inherit = 'helpdesk.ticket'
+    """ _inherit = 'helpdesk.ticket'
     _name = 'helpdesk.change.request'
     reason_for_change = fields.Text('Reason for Change')
     due_date = fields.Date(string="Due Date")
@@ -16,11 +16,10 @@ class ChangeRequest(models.Model):
     priority = fields.Selection(string="Priority",[('1','Standard'),('2','Minor'),('3','Medium'),('4','Critical')])
     cmb_meeting_date = fields.Date(string="CMB Meeting date")
     validation_status = fields.Selection(string="Validation Status",[('1','Approved'),('2','Rejected'),('3','On Hold')])
-
     risk_analysis = fields.Many2one() 
     implementation_testing_plan = fields.Many2one()
     rollback_plan = fields.Many2one()
-    backout_plan = fields.Many2one()
+    backout_plan = fields.Many2one() """
 
 
 
