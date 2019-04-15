1e8014cd5d03fecbb064b9afec411c1e193c2f02
MarwanLaam <laamouri@voisinconsulting.com>
2019-04-15 10:39:15 +0200

    Change Request Finished

--- vcls-helpdesk/models/change_request.py
+++ vcls-helpdesk/models/change_request.py
@@ -12,7 +12,7 @@ class ChangeRequest(models.Model):
     name = fields.Char()
     reason_for_change = fields.Text('Reason for Change')
     due_date = fields.Date(string="Due Date")
-    impact = fields.Integer()
+    impact = fields.Selection([('0.1','10 %'),('0.3','30 %'),('0.5','50 %'),('0.7','70 %')],string="Impact")
     severity = fields.Selection([('1','Low / Informational'),('2','Normal / Minor'),('3','Significant / Critical')],string="Severity")
     main_risks = fields.Text('Main risks')
     priority = fields.Selection([('0','Standard'),('1','Minor'),('2','Medium'),('3','Critical')],string="Priority")
