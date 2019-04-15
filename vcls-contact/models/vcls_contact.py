832b0cccb0dbd0a9b95b3d22e1ac81bd7672e1f0
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-04-15 15:34:08 +0200

    stage creation

--- vcls-contact/models/vcls_contact.py
+++ vcls-contact/models/vcls_contact.py
@@ -5,22 +5,13 @@ from odoo import models, fields, api
 class ContactExt(models.Model):
 
     _inherit = 'res.partner'
+    
+    ### CUSTOM FIELDS FOR EVERY KIND OF CONTACTS ###
 
     hidden = fields.Boolean(
         string="Confidential",
         default=False,
-        )
-
-    stage = fields.Selection([
-        ('1', 'Undefined'),
-        ('2', 'New'),
-        ('3', 'Verified'),
-        ('4', 'Outdated'),
-        ('5', 'Archived'),], 
-        string='Status',
-        track_visibility='onchange',
-        default='1',
-        )
+    )
 
     is_internal = fields.Boolean(
         string="Is Internal",
@@ -28,6 +19,18 @@ class ContactExt(models.Model):
         store = True,
         default = False,
     )
+
+    stage = fields.Selection([
+        ('1', 'Undefined'),
+        ('2', 'New'),
+        ('3', 'Verified'),
+        ('4', 'Outdated'),
+        ('5', 'Archived')], 
+        string='Status',
+        default='1',
+    )
+
+    ### CLIENT RELATED FIELDS ###
 
     ###################
     # COMPUTE METHODS #
@@ -38,3 +41,9 @@ class ContactExt(models.Model):
         for contact in self:
             if contact.employee or self.env['res.company'].search([('partner_id.id','=',contact.id)]):
                 contact.is_internal = True
+
+    # We reset the number of bounced emails to 0 in order to re-detect problems after email change
+    @api.onchange('email')
+    def _reset_bounce(self):
+        for contact in self:
+            contact.message_bounce = 0
