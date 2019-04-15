d916cb3333c77187007eba2809292a8fa0c6ad2b
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-04-15 16:19:00 +0200

    change request cleaning

--- vcls-helpdesk/models/__init__.py
+++ vcls-helpdesk/models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from . import ir_model_data
-#from . import vcls_ticket
+from . import vcls_ticket
 from . import mail_template
-from . import change_request
+#from . import change_request
