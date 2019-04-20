c7253478c0814fe3cccee5df21900015eb0b6c62
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-04-20 23:36:18 +0200

    Added vcls-admin

---
+++ vcls-admin/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "vcls-admin",
+
+    'summary': """
+        VCLS customs for admin applications""",
+
+    'description': """
+    """,
+
+    'author': "VCLS",
+    'website': "http://www.voisinconsulting.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.2.3',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+                    'vcls-hr',
+                    'vcls-helpdesk',
+                    'vcls-contact',
+                    'vcls-crm',
+                    'vcls-interfaces',
+                    'vcls-project',
+                    'vcls-theme'
+                ],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
