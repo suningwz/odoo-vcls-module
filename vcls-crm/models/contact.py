45f085217dc406878a85f4fa3ea3f0be2704a6d7
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-05-06 13:59:26 +0200

    marketing fields created

--- vcls-crm/models/contact.py
+++ vcls-crm/models/contact.py
@@ -9,4 +9,37 @@ class ContactExt(models.Model):
     
     ### CUSTOM FIELDS RELATED TO MARKETING PURPOSES ###
 
+    no_mail = fields.Boolean(
+        default = False,
+        help = "Tick to exclude the contact from emailings.",
+        string = 'Do Not Mail',
+    )
+
+    opted_in = fields.Boolean(
+        default = False,
+        string = 'OptedIn',
+        help = 'Ticked if the client has specifically accepted to receive emailings.',
+    )
+
+    opted_out = fields.Boolean(
+        default = False,
+        string = 'OptedOut',
+        help = 'Ticked if the client has specifically refused to receive emailings.',
+    )
+
     
+
+    external_ref_id = fields.Many2one(
+        'res.partner',
+        string = "External Reference",
+        help = 'Document if the contact has been proposed by another contact.',
+    )
+
+    lead_source_id = fields.Many2one(
+        'lead.source',
+        string='Initial Lead Source',
+    )
+
+    initial_interest = fields.Char(
+        string='Initial Interest',
+    )
