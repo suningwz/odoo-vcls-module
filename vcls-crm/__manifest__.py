8944e0123fcabfa6acc8cf0f791fad251388adea
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-05-08 17:34:18 +0200

    CRM part 1

--- vcls-crm/__manifest__.py
+++ vcls-crm/__manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.2',
+    'version': '0.3',
 
     # any module necessary for this one to work correctly
     'depends': ['base',
@@ -23,7 +23,8 @@
                 #'marketing_automation',
                 #'mass_mailing',
                 'website',
-                #'website_crm',
+                'website_crm',
+                'website_crm_score',
                 'vcls-contact',
                 ],
 
