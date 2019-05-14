1541c2b9d8dbecea9d600629732e4a607a24aa4f
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-05-14 11:41:01 +0200

    partner Relations Structure

--- vcls-crm/models/partner_relation.py
+++ vcls-crm/models/partner_relation.py
@@ -7,13 +7,12 @@ class PartnerRelations(models.Model):
 
     _name = 'res.partner.relation'
     _description = 'Used to map a set of predefined relation types between partners.'
-
-    """relation_type = fields.Selection([
-        ('company_parent', 'Parent Company'),
-        ('company_acquisition', 'Aquired Company'),
-        ('individual_transfer', 'Individual Transfer'),
-        ],
-    )"""
+    
+    type_id = fields.Many2one(
+        'partner.relation.type',
+        string = "Relation Type",
+        required = True,
+    )
     
     source_partner_id = fields.Many2one(
         'res.partner',
@@ -26,3 +25,28 @@ class PartnerRelations(models.Model):
         string = 'Target Partner',
         required = True,
     )
+
+class PartnerRelationType(models.Model):
+
+    _name = 'partner.relation.type'
+    _description = 'Predefined relations between partners.'
+
+    """relation_type = fields.Selection([
+        ('company_parent', 'Parent Company'),
+        ('company_acquisition', 'Aquired Company'),
+        ('individual_transfer', 'Individual Transfer'),
+        ],
+    )"""
+
+    name = fields.Char(
+        required = True,
+    )
+    active = fields.Boolean(
+        default = True,
+    )
+
+    source_message = fields.Char()
+    source_domain  = fields.Char()
+
+    target_message = fields.Char()
+    target_domain  = fields.Char()
