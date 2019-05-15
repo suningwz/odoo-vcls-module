b0adb3a2db70f2a4fc7145d4fb0e9a480c59d7f4
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-05-15 21:32:58 +0200

    Update mapOdoo.py

--- vcls-etl/models/mapOdoo.py
+++ vcls-etl/models/mapOdoo.py
@@ -2,6 +2,8 @@ from odoo import models, fields, api
 
 class mapOdoo(models.Model):
     _name = 'map.odoo'
+    _description = 'used to map external values to Odoo Ids'
+
     odModelName = fields.Char()
     externalName = fields.Char()
     externalOdooId = fields.Char()
