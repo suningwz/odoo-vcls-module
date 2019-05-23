2664e7d07138256e5ed8cbed79c9a1ad6809179c
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-05-23 13:45:40 +0200

    Removed analytic line override

--- vcls-accounting/models/analytic_line.py
+++ vcls-accounting/models/analytic_line.py
@@ -2,7 +2,7 @@
 
 from odoo import models, fields, api
 from odoo.exceptions import UserError, ValidationError
-
+"""
 class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
 
@@ -11,7 +11,7 @@ class AccountAnalyticLine(models.Model):
     def _check_company_id(self):
         # WE temporary deactivate this constrain to ensure leave to be approved by other countries
         pass
-        """
+        
         for line in self:
             if line.account_id.company_id and line.company_id.id != line.account_id.company_id.id:
                 raise ValidationError(_('The selected account belongs to another company that the one you\'re trying to create an analytic item for'))
