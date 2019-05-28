69ba29b63fe0385b21fc14936f65e708f35f7a2e
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-05-28 14:13:40 +0200

    Product Deliverable Created

--- vcls-crm/models/product_template.py
+++ vcls-crm/models/product_template.py
@@ -3,6 +3,22 @@
 from odoo import models, fields, tools, api
 from odoo.exceptions import UserError, ValidationError
 
+class Deliverable(models.Models):
+
+    _name = 'product.deliverable'
+    _description = 'VCLS Deliverable'
+
+    name = fields.Char()
+    active = fields.Boolean(
+        default = True,
+    )
+
+    product_category_id = fields.Many2one(
+        'product.category',
+        string = 'Business Line',
+        domain = "[('parent_id','=',False)]"
+    )
+
 class ProductTemplate(models.Model):
 
     _inherit = 'product.template'
