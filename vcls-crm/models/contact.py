32ba96a552b19e2ef8d2fb3684fbb17bcc2ad3b0
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-05-29 17:00:59 +0200

    default_currency_id with related methods

--- vcls-crm/models/contact.py
+++ vcls-crm/models/contact.py
@@ -30,3 +30,21 @@ class ContactExt(models.Model):
         string = "Initial Contact",
         domain = "[('employee','=',True)]",
     )
+
+    default_currency_id = fields.Many2one(
+        'res.currency',
+        compute='_get_default_currency',
+        inverse='_set_default_currency',
+    )
+    
+    def _get_default_currency(self):
+        ### handle the case where the user's company was created after the currency was set
+        return self.property_product_pricelist.currency_id
+    
+    def _set_default_currency(self, currency):
+        self = self.sudo()
+        pricelist = self.env['product.pricelist'].search([('company_id', '=', False), ('currency_id', '=', currency.id)], limit=1)
+        if not pricelist:
+            raise UserError(_('Please define a company independent pricelist with currency %s') % currency.name)
+        for company in self.env['res.company'].search():
+            self.with_context(force_company=company.id).property_product_pricelist = pricelist
