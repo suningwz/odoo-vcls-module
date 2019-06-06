a187b46ede26126818f530f3d62e3f49d717225b
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-06-06 17:57:32 +0200

    Lead internal ref

--- vcls-crm/models/lead.py
+++ vcls-crm/models/lead.py
@@ -71,6 +71,13 @@ class Leads(models.Model):
 
     won_reason = fields.Many2one('crm.won.reason', string='Won Reason', index=True, track_visibility='onchange')
 
+    internal_ref = fields.Char(
+        string="Ref",
+        readonly = True,
+        store = True,
+        compute = '_compute_internal_ref',
+    )
+
     ###################
     # COMPUTE METHODS #
     ###################
@@ -81,6 +88,16 @@ class Leads(models.Model):
             groups = lead.country_id.country_group_ids
             if groups:
                 lead.country_group_id = groups[0]
+    
+    @api.depends('partner_id')
+    def _compute_internal_ref(self):
+        if self.partner_id:
+            if not self.partner_id.altname:
+                raise UserError("Please document ALTNAME for the client {}".format(self.partner_id.name))
+
+            next_index = self.partner_id.core_process_index+1 or 1
+            self.partner_id.core_process_index = next_index
+            self.internal_ref = "{}-{:03}".format(self.partner_id.altname,next_index)
     
     """@api.onchange('partner_id','country_id')
     def _change_am(self):
