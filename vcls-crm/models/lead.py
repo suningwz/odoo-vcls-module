3dde5161e7cea80f956fc2e501f06cd4d10be113
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-06-12 16:01:23 +0200

    _compute_internal_ref as multi

--- vcls-crm/models/lead.py
+++ vcls-crm/models/lead.py
@@ -96,13 +96,14 @@ class Leads(models.Model):
     
     @api.depends('partner_id')
     def _compute_internal_ref(self):
-        if self.partner_id:
-            if not self.partner_id.altname:
-                raise Warning("Please document ALTNAME for the client {}".format(self.partner_id.name))
+        for lead in self:
+            if lead.partner_id:
+                if not lead.partner_id.altname:
+                    raise Warning("Please document ALTNAME for the client {}".format(lead.partner_id.name))
 
-            next_index = self.partner_id.core_process_index+1 or 1
-            self.partner_id.core_process_index = next_index
-            self.internal_ref = "{}-{:03}".format(self.partner_id.altname,next_index)
+                next_index = lead.partner_id.core_process_index+1 or 1
+                lead.partner_id.core_process_index = next_index
+                lead.internal_ref = "{}-{:03}".format(lead.partner_id.altname,next_index)
 
     ################
     # TOOL METHODS #
