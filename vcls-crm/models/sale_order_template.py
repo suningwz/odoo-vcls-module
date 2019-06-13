51b33e3b496da0ac03b65d2a575af3721a748d87
MarwanLaam <laamouri@voisinconsulting.com>
2019-06-13 16:36:22 +0200

    Create sale_order_template.py

---
+++ vcls-crm/models/sale_order_template.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, tools, api
+from odoo.exceptions import UserError, ValidationError
+
+class SaleOrderTemplate(models.Model):
+
+    _inherit = 'sale.order.template'
+
+    product_category_id = fields.Many2one(
+        'product.category',
+        string = 'Business Line',
+        domain = "[('parent_id','=',False)]"
+    )
