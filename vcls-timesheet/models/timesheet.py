8c9ab0f5bf2a28623290f9392fc228196f91d871
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-06-25 13:31:42 +0200

    added description

--- vcls-timesheet/models/timesheet.py
+++ vcls-timesheet/models/timesheet.py
@@ -4,6 +4,7 @@ from odoo.exceptions import UserError, ValidationError
 
 class TimesheetAdjustment(models.Model):
     _name = 'timesheet.adjustment.reason'
+    _description = 'Timesheet Adjustment Reason'
 
     name = fields.Char()
     active = fields.Boolean()
