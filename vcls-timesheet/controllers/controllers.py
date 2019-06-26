a3db883bf64502edbe0ec8503829f0c3fb567ac6
MarwanLaam <laamouri@voisinconsulting.com>
2019-06-26 15:24:28 +0200

    update Overview

--- vcls-timesheet/controllers/controllers.py
+++ vcls-timesheet/controllers/controllers.py
@@ -1,20 +1,47 @@
-# -*- coding: utf-8 -*-
-from odoo import http
+import babel
+from dateutil.relativedelta import relativedelta
+import itertools
+import json
+from odoo import http, fields, _
+from odoo.http import request
+from odoo.tools import float_round
 
-# class Vcls-timesheet(http.Controller):
-#     @http.route('/vcls-timesheet/vcls-timesheet/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+from odoo.addons.sale_timesheet.controllers.main import SaleTimesheetController
 
-#     @http.route('/vcls-timesheet/vcls-timesheet/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('vcls-timesheet.listing', {
-#             'root': '/vcls-timesheet/vcls-timesheet',
-#             'objects': http.request.env['vcls-timesheet.vcls-timesheet'].search([]),
-#         })
+class TimesheetController(SaleTimesheetController):
 
-#     @http.route('/vcls-timesheet/vcls-timesheet/objects/<model("vcls-timesheet.vcls-timesheet"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('vcls-timesheet.object', {
-#             'object': obj
-#         })
+    def _plan_get_stat_button(self, projects):
+        stat_buttons = super(TimesheetController,self)._plan_get_stat_button(projects)
+
+        stat_buttons.append({
+            'name': _('Report Analysis'),
+            'res_model': 'project.timesheet.forecast.report.analysis',
+            'domain': [('project_id', 'in', projects.ids)],
+            'icon': 'fa fa-calendar',
+        })
+        return stat_buttons
+
+    @http.route('/timesheet/plan/action', type='json', auth="user")
+    def plan_stat_button(self, domain=[], res_model='account.analytic.line', res_id=False):
+        if res_model == 'project.timesheet.forecast.report.analysis':
+            ts_view_graph_id = request.env.ref('project_timesheet_forecast.project_timesheet_forecast_report_view_graph').id
+            ts_view_pivot_id = request.env.ref('project_timesheet_forecast.project_timesheet_forecast_report_view_pivot').id
+            ts_search_view_id = request.env.ref('project_timesheet_forecast.project_timesheet_forecast_report_view_search').id
+            action = {
+                'name': _('Report Analysis'),
+                'type': 'ir.actions.act_window',
+                'res_model': res_model,
+                'view_mode': 'graph,pivot',
+                'views': [[ts_view_graph_id, 'graph'], [ts_view_pivot_id, 'pivot']],
+                'domain': domain,
+                'context' : {
+                    'pivot_row_groupby': ['employee_id'],
+                    'pivot_col_groupby': ['date', 'type'],
+                    'pivot_measures': ['number_hours'],
+                    'group_by': ['date:month', 'type'],
+                    'search_default_year': True,},
+                'search_view_id': ts_search_view_id,
+            }
+        else:
+            action = super(TimesheetController,self).plan_stat_button(domain,res_model,res_id)
+        return action 
