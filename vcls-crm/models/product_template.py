9fc65ebb9f94a96f2309ae49344c5e26107ceb09
MarwanLaam <laamouri@voisinconsulting.com>
2019-06-28 11:54:49 +0200

    update product_template.py

--- vcls-crm/models/product_template.py
+++ vcls-crm/models/product_template.py
@@ -37,3 +37,22 @@ class ProductTemplate(models.Model):
         'product.deliverable',
         string = 'Deliverable',
     )
+    
+class Product(models.Model):
+
+    _inherit = 'product.product'
+    @api.model
+    def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
+        business_mode = self._context.get('business_mode')
+        business_line = self._context.get('business_line')
+        deliverable_id = self._context.get('deliverable_id')
+        product_ids = super(Product, self)._search(args, offset, limit, order, count=count, access_rights_uid=access_rights_uid)
+        products = self.browse(product_ids)
+        if business_mode and business_line and deliverable_id:
+            products = products.filtered(lambda p: business_line in p.categ_id.ids and deliverable_id in p.deliverable_id.ids)
+            if business_mode == 'fixed_price':
+                products = products.filtered(lambda p: p.invoice_policy == 'order' and p.expense_policy == 'sales_price')
+            elif business_mode == 't_and_m':
+                products = products.filtered(lambda p: p.invoice_policy == 'order' and p.expense_policy == 'no' and p.seniority_level_id)
+        print(product_ids)
+        return products.ids
