8c2c19b53560c0ebb5e5b5da41d653eee8deea0c
MarwanLaam <laamouri@voisinconsulting.com>
2019-06-28 11:52:31 +0200

    update sale_order.py

--- vcls-crm/models/sale_order.py
+++ vcls-crm/models/sale_order.py
@@ -16,6 +16,16 @@ class SaleOrder(models.Model):
         domain = "[('is_business_line','=',True)]"
     )
 
+    business_mode = fields.Selection([
+        ('t_and_m', 'T&M'), 
+        ('fixed_price', 'Fixed Price'), 
+        ], default='t_and_m')
+
+    deliverable_id = fields.Many2one(
+        'product.deliverable',
+        string="Deliverable"
+    )
+
     @api.model
     def create(self, vals):
 
@@ -33,3 +43,23 @@ class SaleOrder(models.Model):
 
         result = super(SaleOrder, self).create(vals)
         return result
+
+class Product(models.Model):
+
+    _inherit = 'product.product'
+    @api.model
+    def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
+        business_mode = self._context.get('business_mode')
+        business_line = self._context.get('business_line')
+        deliverable_id = self._context.get('deliverable_id')
+        product_ids = super(Product, self)._search(args, offset, limit, order, count=count, access_rights_uid=access_rights_uid)
+        products = self.browse(product_ids)
+        if business_mode and business_line and deliverable_id:
+            products = products.filtered(lambda p: business_line in p.categ_id.ids and deliverable_id in p.deliverable_id.ids)
+            if business_mode == 'fixed_price':
+                products = products.filtered(lambda p: p.invoice_policy == 'order' and p.expense_policy == 'sales_price')
+            elif business_mode == 't_and_m':
+                products = products.filtered(lambda p: p.invoice_policy == 'order' and p.expense_policy == 'no' and p.seniority_level_id)
+        print(product_ids)
+        return products.ids
+        
