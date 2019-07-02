023b7fcd4169b1e26a0d92efdf24893d7ba876ae
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-07-02 16:18:12 +0200

    Product Search II

--- vcls-crm/models/product_template.py
+++ vcls-crm/models/product_template.py
@@ -46,6 +46,46 @@ class Product(models.Model):
     _inherit = 'product.product'
     @api.model
     def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
+        vcls_search = self._context.get('vcls_search')
+
+        #if we are in the context of a vcls custom search
+        if vcls_search:
+            business_mode = self._context.get('business_mode')
+            business_line = self._context.get('business_line')
+            deliverable_id = self._context.get('deliverable_id')
+
+            product_ids = super(Product, self)._search(args, offset, None, order, count=count, access_rights_uid=access_rights_uid)
+            products = self.browse(product_ids)
+            _logger.info("Custom VCLS product search start with {} to filter with mode {}, business line {} and deliverable {}".format(len(products),business_mode,business_line,deliverable_id))
+            
+            if business_mode:
+                #If Fixed Price, Show only products with invoicing policy based on milestones and a re-invoicing policy configured as sales price
+                if business_mode == 'fixed_price':
+                    products = products.filtered(lambda p: p.invoice_policy == 'delivered_manual' and p.expense_policy == 'sales_price')
+                #If T&M, Show Services (i.e. milestones and re-invoicing = NO) and rates products (with a seniority level not null)
+                elif business_mode == 't_and_m':
+                    products = products.filtered(lambda p: (p.invoice_policy == 'delivered_manual' and p.expense_policy == 'no') or (p.expense_policy == 'no' and p.seniority_level_id))
+                
+                _logger.info("SEARCH found {} for mode {}".format(len(products),business_mode))
+
+            if business_line:
+                bl_childs = self.env['product.category'].search([('id','child_of',business_line)])
+                products = products.filtered(lambda p: p.categ_id in bl_childs)
+                _logger.info("SEARCH found {} in {}".format(len(products),bl_childs.mapped('name')))
+
+            if deliverable_id:
+                products = products.filtered(lambda p: p.deliverable_id == deliverable_id)
+                _logger.info("SEARCH found {} for product {}".format(len(products),deliverable_id))
+            
+            return products.ids
+
+        else:
+            product_ids = super(Product, self)._search(args, offset, limit, order, count=count, access_rights_uid=access_rights_uid)
+            return product_ids
+
+
+
+        """
         business_mode = self._context.get('business_mode')
         business_line = self._context.get('business_line')
         deliverable_id = self._context.get('deliverable_id')
@@ -60,7 +100,7 @@ class Product(models.Model):
             _logger.info("SEARCH BL cat: {} ".format(bl_childs.mapped('name')))
             products = products.filtered(lambda p: p.categ_id in bl_childs)
 
-        """if business_line:
+        if business_line:
             business_line_child_ids = self.env['product.category'].browse(business_line).child_id.ids
             if business_line_child_ids:
                 products = products.filtered(lambda p: p.categ_id.ids in business_line_child_ids)
@@ -73,5 +113,6 @@ class Product(models.Model):
             #If T&M, Show Services (i.e. milestones and re-invoicing = NO) and rates products (with a seniority level not null)
             elif business_mode == 't_and_m':
                 products = products.filtered(lambda p: (p.invoice_policy == 'delivered_manual' and p.expense_policy == 'no') or (p.expense_policy == 'no' and p.seniority_level_id))
-        """
+        
         return products.ids
+        """
