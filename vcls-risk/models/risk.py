c10076e62b13c87f3ecd3acccf199790a77f9af2
MarwanLaam <laamouri@voisinconsulting.com>
2019-07-02 17:09:28 +0200

    Risk mgmt implementation: part II

--- vcls-risk/models/risk.py
+++ vcls-risk/models/risk.py
@@ -24,3 +24,31 @@ class RiskType(models.Model):
     weight = fields.Integer(default = 1)
     
     category = fields.Char()
+
+class Risk(models.Model):
+    _name = 'risk'
+    #_inherit = ['mail.thread', 'mail.activity.mixin', 'mail.blacklist.mixin', 'portal.mixin', 'mail.alias.mixin']
+    _description = 'A Risk'
+
+    risk_type_id = fields.Many2one('risk.type',
+                                    string = "Type of risk",
+                                    required = True)
+
+    note = fields.Char()
+
+    resource = fields.Char(string='Resource', index=True, help="If not set, acts as a default value for new resources")
+
+    risk_level = fields.Selection([(1, 'Low'), (2, 'Moderate'), (3, 'Significant'), (4, 'High')], 'Risk Level',)
+
+    last_notification = fields.Datetime(readonly = True)
+
+    score = fields.Integer(string = "Score", compute="_compute_score")
+
+    @api.depends('risk_level', 'risk_type_id.weight')
+    def _compute_score(self):
+        for risk in self:
+            if risk.risk_level:
+                risk.score = risk.risk_level * risk.risk_type_id.weight
+            else:
+                risk.score = 0
+
