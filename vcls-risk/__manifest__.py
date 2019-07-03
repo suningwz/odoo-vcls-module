2a2d286e78963fca3d572177af91b4d6bb0349b0
MarwanLaam <laamouri@voisinconsulting.com>
2019-07-03 09:53:43 +0200

    Add inheritance

--- vcls-risk/__manifest__.py
+++ vcls-risk/__manifest__.py
@@ -18,7 +18,8 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base',
+                'mail'],
 
     # always loaded
     'data': [
