3e3d6ba77c9eb39a8ee2b8f96db42c68cecae026
MarwanLaam <laamouri@voisinconsulting.com>
2019-07-03 15:04:23 +0200

    Update vcls-risk

--- vcls-risk/models/sale.py
+++ vcls-risk/models/sale.py
@@ -6,13 +6,13 @@ from odoo.exceptions import UserError, ValidationError
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-    risk_ids = fields.Many2many('risk', string='Risk')
+    risk_id = fields.Many2one('risk', string='Risk')
 
     def action_risk(self):
         view_ids = [self.env.ref('vcls-risk.view_risk_tree').id,
                     self.env.ref('vcls-risk.view_risk_kanban').id, 
                     self.env.ref('vcls-risk.view_risk_form').id ]
-        risk_ids = self.risk_ids
+        risk_id = self.risk_id
 
         return {
             'name': 'All Risks',
@@ -22,7 +22,7 @@ class SaleOrder(models.Model):
             'target': 'current',
             'res_model': 'risk',
             'type': 'ir.actions.act_window',
-            'context': {'search_default_id': risk_ids.ids,},
+            'context': {'search_default_id': risk_id.id,},
         } 
 
 class SaleOrderLine(models.Model):
@@ -30,24 +30,20 @@ class SaleOrderLine(models.Model):
 
     def create_risk(self):
         so_line_price = self.price_unit
-        product_price = self.product_id.list_price
+        product_prices = self.product_id.item_ids
 
-        if product_price != so_line_price:
-            resource ='sale.order.line,'+str(self.id)
-            risk = self.env['risk'].search([('resource', '=', resource)])
-            if not risk:
-                risk_type = self.env['risk.type'].create({'name': 'RiskTest', 'active': True, 'model_name': 'sale.order.line'})
-                risk = self.env['risk'].create({'risk_type_id': risk_type.id, 'resource': resource}).id
-                self.order_id.risk_ids = [(4, risk)]
-
-        """ product_prices = self.product_id.pricelist_item_ids
-        for product_price in product_prices:
-            if product_price.fixed_price == so_line_price:
-                risk_type = self.env['risk.type'].create({'name': 'RiskTest', 'active': True, 'model_name': 'sale.order.line'})
-                risk = self.env['risk'].create({'risk_type_id': risk_type.id, })
-                self.order_id.risk_ids = [(6, 0, risk.id)] """
+        if self.product_id.seniority_level_id:
+            for product_price in product_prices:
+                if product_price.pricelist_id == self.pricelist_id and product_price.fixed_price != so_line_price:
+                    risk_type = self.env['risk.type'].search([('name', '=', 'Sale Order Risk'), ('model_name', '=', 'sale.order.line')])
+                    resource ='sale.order.line,' + str(self.id)
+                    risk = self.env['risk'].search([('resource', '=', resource)])
+                    if not risk_type:
+                        risk_type = self.env['risk.type'].create({'name': 'Sale Order Risk', 'active': True, 'model_name': 'sale.order.line'})
+                    if not risk:
+                        risk = self.env['risk'].create({'risk_type_id': risk_type.id, 'resource': resource}).id
+                        self.order_id.risk_id = risk
         
-
     @api.multi
     def write(self, vals):
         result = super(SaleOrderLine, self).write(vals)
