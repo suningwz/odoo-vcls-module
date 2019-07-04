54772618e0b2f62ad48e98daeb3d327bb2a1608e
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-07-04 13:36:17 +0200

    po refinements

--- vcls-invoicing/__manifest__.py
+++ vcls-invoicing/__manifest__.py
@@ -26,8 +26,6 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
         'views/sale_views.xml',
         'views/invoice_views.xml',
     ],
