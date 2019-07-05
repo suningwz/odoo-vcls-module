d0b91b4828895ecb75ceb4003da5472e35ae9892
MarwanLaam <laamouri@voisinconsulting.com>
2019-07-05 16:02:11 +0200

    Update contact.py

--- vcls-invoicing/models/contact.py
+++ vcls-invoicing/models/contact.py
@@ -8,6 +8,25 @@ class Contact(models.Model):
     _inherit = 'res.partner'
     risk_ids = fields.Many2many('risk', string='Risk')
 
+    communication_rate = fields.Selection([ (0.0, '0%'), 
+                                            (0.05, '0.5%'), 
+                                            (0.1, '1%'), 
+                                            (0.15, '1.5%'), 
+                                            (0.2, '2%'), 
+                                            (0.25, '2.5%'), 
+                                            (0.3, '3%'), 
+                                            ], 'Communication Rate', default = 0.0)
+    
+    invoicing_frequency = fields.Selection([('month','Month'),
+                                            ('trimester','Trimester'),
+                                            ('milestone','Miliestone')], default='month')
+    
+    outsourcing_permission = fields.Boolean(default=False)
+
+    invoice_template = fields.Many2one('ir.actions.report', domain=[('model','=','account.invoice')])
+
+    activity_report_template = fields.Many2one('ir.actions.report', domain=[('model','=','account.analytic.line'), ('report_name','=','activity_report')])
+
     def action_risk(self):
         view_ids = [self.env.ref('vcls-risk.view_risk_tree').id,
                     self.env.ref('vcls-risk.view_risk_kanban').id, 
