09660053bd3c274ac4547656a127b2fd6322dde9
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-07-09 15:41:01 +0200

    Template emp menu

--- vcls-hr/__manifest__.py
+++ vcls-hr/__manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '1.6.3',
+    'version': '1.6.4',
 
     # any module necessary for this one to work correctly
     'depends': ['base',
