65bdb6b96914436bc00a1e2e0a4aa8120f497385
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-07-23 08:13:53 +0200

    Timesheet Limit Date

--- vcls-invoicing/__manifest__.py
+++ vcls-invoicing/__manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '1.0.1',
+    'version': '1.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base',
@@ -23,7 +23,9 @@
                 'vcls-contact',
                 'vcls-risk',
                 'vcls-hr',
-                'account',],
+                'account',
+                'sale_timesheet_limit_date',
+                ],
 
     # always loaded
     'data': [
