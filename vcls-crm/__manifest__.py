b7f6d86247b45593f53500e3ede705c12c6ce6b5
Khuong <vo@voisinconsulting.com>
2019-07-25 11:44:48 +0200

    Update __manifest__.py

--- vcls-crm/__manifest__.py
+++ vcls-crm/__manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '1.3',
+    'version': '1.4',
 
     # any module necessary for this one to work correctly
     'depends': ['base',
@@ -24,6 +24,7 @@
                 #'mass_mailing',
                 'website',
                 'website_crm',
+                'partner_firstname',
                 'website_crm_score',
                 'vcls-contact',
                 'sale_management',
