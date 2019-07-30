869cc1925c7bc53bb053c923ae9bad02f65ca581
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-07-30 14:00:15 +0200

    Update time_category.py

--- vcls-timesheet/models/time_category.py
+++ vcls-timesheet/models/time_category.py
@@ -1,6 +1,10 @@
 from odoo import models, fields, tools, api
 from odoo.exceptions import UserError, ValidationError
 
+import logging
+_logger = logging.getLogger(__name__)
+
+
 class TimeCategory(models.Model):
     _name = 'project.time_category'
 
@@ -15,9 +19,10 @@ class TimeCategory(models.Model):
         task_id = self._context.get('task_id')
 
         time_categories_ids = super(TimeCategory, self)._search(args, offset, limit, order, count=count, access_rights_uid=access_rights_uid)
+        _logger.info("{} | {}".format(task_id,time_categories_ids))
 
         #if we are in the context of a custom task filtered search, we look at the list of authorized categories in the related task
         if task_id:
-            time_categories_ids.filtered(lambda r: r.id in task_id.time_categories_ids)
+            time_categories_ids.filter(lambda r: r.id in task_id.time_categories_ids)
             
         return time_categories_ids
