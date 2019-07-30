ce9ede6f1ee3a42eebe2d7a170eb1928d49cb2f4
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-07-30 14:26:05 +0200

    consolidated timecat

--- vcls-timesheet/models/time_category.py
+++ vcls-timesheet/models/time_category.py
@@ -21,9 +21,11 @@ class TimeCategory(models.Model):
         time_category_ids = super(TimeCategory, self)._search(args, offset, limit, order, count=count, access_rights_uid=access_rights_uid)
 
         #if we are in the context of a custom task filtered search, we look at the list of authorized categories in the related task
-        if task_id:
-            task = self.env['project.task'].browse(task_id)
-            #_logger.info("task {} | all tc {} | found {} with tc {}".format(task_id,time_category_ids,task.id,task.time_category_ids))
-            return task.time_category_ids.mapped('id')
+        if self._context.get('task_filter'):
+            if task_id:
+                task = self.env['project.task'].browse(task_id)
+                return task.time_category_ids.mapped('id')
+            else:
+                return []
             
         return time_category_ids
