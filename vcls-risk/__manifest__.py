7316a35555f8c2ab7ea09b230867a0682d028948
Khuong <vo@voisinconsulting.com>
2019-08-07 15:55:30 +0200

    Go/NoGo required (only send risk)

--- vcls-risk/__manifest__.py
+++ vcls-risk/__manifest__.py
@@ -15,11 +15,13 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '1.0.1',
+    'version': '1.1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base',
-                'mail',],
+                'mail',
+                'vcls-crm',
+                ],
 
     # always loaded
     'data': [
