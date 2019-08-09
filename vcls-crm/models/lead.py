3fb8d0fa65614db7d09fb61e24f16381191ce433
Khuong <vo@voisinconsulting.com>
2019-08-09 13:55:30 +0200

    future discard

--- vcls-crm/models/lead.py
+++ vcls-crm/models/lead.py
@@ -112,6 +112,13 @@ class Leads(models.Model):
         track_visibility='onchange'
     )
 
+    won_reason = fields.Many2one(
+        'crm.won.reason',
+        string='Won Reason',
+        index=True,
+        track_visibility='onchange'
+    )
+
     lost_reasons = fields.Many2many(
         'crm.lost.reason',
         string='Lost Reasons',
