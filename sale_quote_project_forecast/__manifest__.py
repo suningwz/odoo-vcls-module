27c018922460c41dd50591e9c014ece6582b170a
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-08-13 12:08:52 +0200

    Revert "Revert "Merge pull request #192 from VCLS-org/12.0_vcls_quotation_change_management""
    This reverts commit 08775205b4ea8cb15227b0988f004770cd56ed55.

--- sale_quote_project_forecast/__manifest__.py
+++ sale_quote_project_forecast/__manifest__.py
@@ -3,14 +3,14 @@
 {
     'name': 'Sale quote project forcast',
     'description': "create a quote from a project forcast",
-    'version': '12.0.4.0.1',
+    'version': '12.0.4.0.2',
     'author': 'Camptocamp',
     'license': 'AGPL-3',
     'category': 'Others',
     'depends': [
         'sale_project_timesheet_by_seniority',
         'project_timesheet_forecast',
-        'sale_milestone_profile_invoicing'
+        'sale_milestone_profile_invoicing',
     ],
     'website': 'http://www.camptocamp.com',
     'data': [
