628afbc43d4ab1ddac868ee1db5a638bda4bf9a5
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-08-15 11:58:52 +0200

    project_id auto add in so.line

--- vcls-crm/models/sale_order_line.py
+++ vcls-crm/models/sale_order_line.py
@@ -16,6 +16,14 @@ class SaleOrderLine(models.Model):
         project = super(SaleOrderLine, self)._timesheet_create_project()
         project.update({'project_type': 'client'})
         return project
+
+    #We override the line creation in order to link them with existing project
+    @api.model
+    def create(self,vals):
+        line = super(SaleOrderLine, self).create(vals)
+        if (line.product_id.service_tracking in ['project_only', 'task_new_project']) and not line.product_id.project_template_id:
+            line.project_id = line.order_id.project_id
+
     
 
     
