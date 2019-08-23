61eed46f8e423f8053fa6d987d2bdc01876ae170
Khuong <vo@voisinconsulting.com>
2019-08-23 11:42:10 +0200

    Customize manifest of expenses

--- vcls-expenses/__manifest__.py
+++ vcls-expenses/__manifest__.py
@@ -3,30 +3,31 @@
     'name': "vcls-expenses",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        VCLS custom expenses module
+        """,
 
     'description': """
-        Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "VCLS",
+    'website': "https://voisinconsulting.com/",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': [
+        'base',
+        'hr_expense',
+    ],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
