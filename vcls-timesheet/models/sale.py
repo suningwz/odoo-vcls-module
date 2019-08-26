6f2a0b56645bebb3662f9ea8658661f553e887a0
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2019-08-26 15:41:43 +0200

    fixup! [FIX] which TS are considered for milestones amount calculation

--- vcls-timesheet/models/sale.py
+++ vcls-timesheet/models/sale.py
@@ -71,7 +71,9 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     def _get_timesheet_for_amount_calculation(self, only_invoiced=False):
-        timesheets = super()._get_timesheet_for_amount_calculation()
+        timesheets = super()._get_timesheet_for_amount_calculation(only_invoiced=only_invoiced)
+        if not timesheets:
+            return timesheets
         timesheets = self.env['account.analytic.line'].search(
             [('id', 'in', timesheets.ids),
              ('state', '=', 'validated'),
