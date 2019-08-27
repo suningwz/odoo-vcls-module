02a5f26f4b8a986d8d6420c0651d6bad16ac73b6
Emmanuel HURET <huret.emmanuel@infoatoutprix.fr>
2019-08-27 22:04:44 +0200

    [IMP] little improvement

--- vcls-expenses/models/expense_sheet.py
+++ vcls-expenses/models/expense_sheet.py
@@ -54,10 +54,18 @@ class ExpenseSheet(models.Model):
             else:
                 if record.employee_id:
                     record.user_id = record.employee_id.parent_id.user_id
-    
+
+    @api.onchange('project_id')
+    def change_project(self):
+        for rec in self:
+            if rec.project_id and rec.project_id.analytic_account_id:
+                rec.analytic_account_id = rec.project_id.analytic_account_id.id
+
     @api.multi
     def open_pop_up_add_expense(self):
         for rec in self:
             action = self.env.ref('vcls-expenses.action_pop_up_add_expense').read()[0]
-            action['context'] = {'default_employee_id': rec.employee_id.id, 'default_analytic_account_id': rec.analytic_account_id.id}
+            action['context'] = {'default_emplouee_id': rec.employee_id.id,
+                                 'default_analytic_account_id': rec.analytic_account_id.id,
+                                 'default_sheet_id': rec.id}
             return action
