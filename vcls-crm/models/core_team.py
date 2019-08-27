77954d3cb30b733478e6cc8ae509bd9ae253e9f3
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-08-27 13:53:55 +0200

    core team button & View

--- vcls-crm/models/core_team.py
+++ vcls-crm/models/core_team.py
@@ -11,15 +11,17 @@ class CoreTeam(models.Model):
     lead_consultant = fields.Many2one(
         'hr.employee',
         string='Lead Consultant',
-        required = True,
        )
 
     consultant_ids = fields.Many2many(
         'hr.employee',
         string='Consultants')
+
     ta_ids = fields.Many2many(
         'hr.employee',
         string='Ta')
+    
+    comment = fields.Char()
     
 class SaleOrder(models.Model):
 
@@ -29,6 +31,22 @@ class SaleOrder(models.Model):
         'core.team',
         string = "Core Team"
     )
+
+    def core_team(self):
+        view_id = self.env.ref('vcls-crm.view_core_team_form').id
+
+        if not self.core_team_id: #if core team not defined by parent, then we create a default one
+            self.core_team_id = self.env['core.team'].create()
+
+        return {
+            'name': 'Core Team',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'target': self.core_team_id,
+            'res_model': 'core.team',
+            'view_id': view_id,
+            'type': 'ir.actions.act_window',
+        }
 
 class Project(models.Model):
 
