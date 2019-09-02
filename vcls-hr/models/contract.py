f7fc5db70a80a0ba9053a1bc8bbdc1b28a15c5c0
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-09-02 14:26:04 +0200

    fixup!

--- vcls-hr/models/contract.py
+++ vcls-hr/models/contract.py
@@ -100,9 +100,9 @@ class Contract(models.Model):
     def write(self,vals):
         if vals.get('job_id',False): #if the job has been updated
             #we grab the employee then raise the ticket
-            employee = self.env['hr.employee'].browse(vals.get('employee_id',self.employee_id))
+            employee = self.env['hr.employee'].browse(vals.get('employee_id',self.employee_id.id))
             employee.create_IT_ticket('job_title_changed')
-            _logger.info("NEW JOB CHANGED TICKET CREATED: {}".format(rec.employee_id.name))
+            _logger.info("NEW JOB CHANGED TICKET CREATED: {}".format(employee.employee_id.name))
         
         return super().write(vals)
 
