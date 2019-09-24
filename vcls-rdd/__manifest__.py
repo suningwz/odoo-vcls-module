1f119b8b25eaca2abfae5fb63e66f32a4b5df49d
mosal <mounir.salim@smile.fr>
2019-09-24 13:29:38 +0200

    [RDD]: Add smile_data_integration and init vcls-init

---
+++ vcls-rdd/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "vcls-rdd",
+
+    'summary': """
+        VCLS SalesForce Odoo migration""",
+
+    'description': """
+    """,
+
+    'author': "VCLS",
+    'website': "https://voisinconsulting.com/",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['vcls-crm',
+                'smile_data_integration'
+            ],
+
+    # always loaded
+    'data': [
+
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+
+    ],
+}
