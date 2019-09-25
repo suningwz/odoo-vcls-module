e43aca69b27221ad094cb30b4e2bc5fcb4605958
mosal <mounir.salim@smile.fr>
2019-09-25 16:10:21 +0200

    [Fix]: add web_grid_extend for odoo.sh in vcls-timesheet dependancies

--- vcls-timesheet/__manifest__.py
+++ vcls-timesheet/__manifest__.py
@@ -16,7 +16,7 @@
     # for the full list
     'category': 'Uncategorized',
 
-    'version': '0.3',
+    'version': '0.3.1',
 
 
     # any module necessary for this one to work correctly
@@ -32,7 +32,7 @@
         'vcls-crm',
         'vcls-project',
         'timesheet_grid',
-        # 'web_grid_extend',
+        'web_grid_extend',
         ],
 
     # always loaded
