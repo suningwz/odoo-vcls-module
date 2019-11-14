0d6131e856679beb6f3a38b788c42b7e6048e4f2
Azer ghadhoun <ghahdoun@voisinconsulting.com>
2019-11-14 15:29:39 +0100

    [FIX] #825726 consultant role

--- vcls_security/model/purchase.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models, tools, _
-from odoo.exceptions import AccessError
-
-
-class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
