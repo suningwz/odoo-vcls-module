75c5eac97b0f53d19a329c781f44393a130a856b
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-11-19 21:28:47 +0100

    [ADD] vcls-rdd

---
+++ vcls-rdd/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "vcls-rdd",
+
+    'summary': """
+        VCLS SalesForce Odoo migration""",
+
+    'description': """
+    """,
+
+    'author': "VCLS",
+    'website': "https://voisinconsulting.com/",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.5',
+
+    # any module necessary for this one to work correctly
+    'depends': ['vcls-crm',
+                'smile_data_integration',
+                'office365_framework',
+            ],
+
+    # always loaded
+    'data': [
+
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+
+    ],
+}
---
+++ vcls-rdd/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from . import sale_order
+from . import agreement
+from . import project
+from . import invoicing_po
+from . import lead
+from . import azure_ad_change_queuer
