884c1bdff7b17c90f2118fccda82200f7e009c94
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-11-21 12:12:11 +0100

    Invoicing Wizard I

--- vcls-invoicing/wizard/sale_make_invoice_advance.py
+++ vcls-invoicing/wizard/sale_make_invoice_advance.py
@@ -1,19 +1,36 @@
 from odoo import api, fields, models, _
 from itertools import groupby
 
+import logging
+_logger = logging.getLogger(__name__)
 
 class SaleAdvancePaymentInv(models.TransientModel):
     _inherit = "sale.advance.payment.inv"
 
-    group_invoice_method = fields.Selection([('one', 'Only this order'),
-                                             ('project', 'project'),
-                                             ('program', 'program'),
-                                             ('agreement', 'agreement'),],string='Grouping Invoice by', default='one')
+    group_invoice_method = fields.Selection([
+        #('one', 'Only this order'),
+        ('project', 'Project and Extensions'),
+        ('program', 'Program'),
+        #('agreement', 'agreement'),
+        ],
+        string='Grouping Invoice by', default='project')
     
     @api.multi
     def create_invoices(self):
         context = self._context.copy()
+
+        active_orders = self.env['sale.order'].browse(self._context.get('active_ids', []))
+        related_orders = active_orders
+        pos = self.env['invoicing.po']
+
         if self.group_invoice_method == 'project':
+            for order in active_orders:
+                related_orders |= order.parent_id | order.parent_sale_order_id | order.child_ids
+                pos |=  order.po_id | order.parent_id.po_id | order.parent_sale_order_id.po_id | order.child_ids.po_id
+
+        _logger.info(" Found orders {} and POs {}".format(related_orders.mapped('name'),pos.mapped('name')))
+
+        """if self.group_invoice_method == 'project':
             projects = self.env['sale.order'].browse(self._context.get('active_ids', [])).mapped('project_ids')
             for project in projects:
                 projects_to_invoice_ids = [p.id for p in self.get_projects(project)]
@@ -31,7 +48,7 @@ class SaleAdvancePaymentInv(models.TransientModel):
         elif self.group_invoice_method == 'agreement':
             agreements = self.env['sale.order'].browse(self._context.get('active_ids', [])).mapped('agreement_id')
             sale_orders = self.env['sale.order'].search([('agreement_id', 'in', agreements.ids)])
-            context['active_ids'] = sale_orders.ids
+            context['active_ids'] = sale_orders.ids"""
 
         return super(SaleAdvancePaymentInv, self.with_context(context)).create_invoices()
 
