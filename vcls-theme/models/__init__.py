59e4b2203342d5673c1ed9c8abf37667fa72965e
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-11-27 12:30:21 +0100

    ToDo app II

--- vcls-theme/models/__init__.py
+++ vcls-theme/models/__init__.py
@@ -3,3 +3,4 @@
 from . import models
 from . import patch_field_view
 from . import ir_ui_menu
+from . import mail_activity
---
+++ vcls-theme/models/mail_activity.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import datetime
+from odoo import models, fields, api, http
+
+from odoo.exceptions import UserError, ValidationError
+
+class MailActivity(models.Model):
+    
+    _inherit = 'mail.activity'
+
+    def go_to_record(self):
+        self.ensure_one()
+        url = http.request.env['ir.config_parameter'].get_param('web.base.url')
+        link = "{}/web#id={}&model={}".format(url, self.res_id, self.res_model)
+        return {
+            'type': 'ir.actions.act_url',
+            'url': link,
+            'target': 'self',
+        }
