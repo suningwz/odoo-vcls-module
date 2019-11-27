04863002a3c2e680b50f3d90ea9f02b3b49a2a05
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-11-27 13:57:03 +0100

    Team ToDo

--- vcls-theme/models/mail_activity.py
+++ vcls-theme/models/mail_activity.py
@@ -8,6 +8,20 @@ class MailActivity(models.Model):
     
     _inherit = 'mail.activity'
 
+    lm_ids = fields.Many2many(
+        'res.users',
+        compute='_get_lm_ids',
+        default = False
+    )
+
+    @api.depends('user_id')  
+    def _get_lm_ids(self):
+        """ Populate a list of authorized user for domain filtering """
+        for rec in self:
+            empl = self.env['hr.employee'].search([('user_id','=',self._uid)])
+            if empl:
+                rec.lm_ids = empl.lm_ids
+
     def go_to_record(self):
         self.ensure_one()
         url = http.request.env['ir.config_parameter'].get_param('web.base.url')
