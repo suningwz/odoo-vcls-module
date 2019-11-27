485a5fc19b213445837dce97fd5dc1c71f697af9
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-11-27 16:20:49 +0100

    Update mail_activity.py

--- vcls-theme/models/mail_activity.py
+++ vcls-theme/models/mail_activity.py
@@ -17,11 +17,13 @@ class MailActivity(models.Model):
 
     @api.depends('user_id')  
     def _get_lm_ids(self):
-        """ Populate a list of authorized user for domain filtering """
+        """ Populate a list of authorized user for domain filtering 
         for rec in self:
             empl = self.env['hr.employee'].search([('user_id','=',rec.user_id.id)])
             if empl:
                 rec.write({'lm_ids':[(6,0,empl.lm_ids.mapped('id'))]})
+        """
+        return False
 
     def go_to_record(self):
         self.ensure_one()
