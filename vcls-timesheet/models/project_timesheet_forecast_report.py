f09b45425a8c0a7d14d3340c2198ccf52ef580c9
Azer ghadhoun <ghahdoun@voisinconsulting.com>
2019-11-28 16:46:26 +0100

    [IMPL] VC.004661order in follow up report
    In the view follow up report, get the stages in the following order :
    
    0. Draft
    1. LC review
    2. PC review
    3. Adjustment Validation
    4. Invoiceable
    5. Invoiced
    ...

--- vcls-timesheet/models/project_timesheet_forecast_report.py
+++ vcls-timesheet/models/project_timesheet_forecast_report.py
@@ -12,9 +12,9 @@ class TimesheetForecastReport(models.Model):
         ('lc_review', '1. LC review'), 
         ('pc_review', '2. PC review'), 
         ('carry_forward', 'Carry Forward'),
-        ('adjustment_validation', '4. Adjustment Validation'),
-        ('invoiceable', '5. Invoiceable'),
-        ('invoiced', '6. Invoiced'),
+        ('adjustment_validation', '3. Adjustment Validation'),
+        ('invoiceable', '4. Invoiceable'),
+        ('invoiced', '5. Invoiced'),
         ('outofscope', 'Out Of Scope'),
     ], 'Stage', readonly=True)
 
@@ -121,3 +121,20 @@ class TimesheetForecastReport(models.Model):
                 )
             )
         """ % (self._table,))
+
+    @api.model
+    def _read_group_prepare(self, orderby, aggregated_fields, annotated_groupbys, query):
+        groupby_terms, orderby_terms = super(TimesheetForecastReport, self)._read_group_prepare(
+            orderby, aggregated_fields, annotated_groupbys, query
+        )
+        if '"stage_id"' in orderby_terms:
+            orderby_terms[orderby_terms.index('"stage_id"')] = """
+                CASE WHEN "{table}"."stage_id" = 'draft' THEN 0 
+                 WHEN "{table}"."stage_id" = 'lc_review' THEN 1  
+                 WHEN "{table}"."stage_id" = 'pc_review' THEN 2 
+                 WHEN "{table}"."stage_id" = 'adjustment_validation' THEN 3 
+                 WHEN "{table}"."stage_id" = 'invoiceable' THEN 4 
+                 WHEN "{table}"."stage_id" = 'invoiced' THEN 5 
+                 ELSE 6 END
+            """.format(table=self._table)
+        return groupby_terms, orderby_terms
