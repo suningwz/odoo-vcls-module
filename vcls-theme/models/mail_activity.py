c0377e0901a4555730fc14285771a246b7349961
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-11-28 11:14:50 +0100

    GO to record renamed

--- vcls-theme/models/mail_activity.py
+++ vcls-theme/models/mail_activity.py
@@ -22,9 +22,9 @@ class MailActivity(models.Model):
             if empl:
                 rec.write({'lm_ids':[(6,0,empl.lm_ids.mapped('id'))]})
         return False"""
-        
+
     @api.multi
-    def go_to_record(self):
+    def _go_to_record(self):
         self.ensure_one()
         url = http.request.env['ir.config_parameter'].get_param('web.base.url')
         link = "{}/web#id={}&model={}".format(url, self.res_id, self.res_model)
