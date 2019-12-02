eb8cfb20d4865e2b7227411703369b5c8a9bc044
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-12-02 16:45:28 +0100

    [FIX] VC.004686 + VC.004684

--- vcls-theme/models/mail_activity.py
+++ vcls-theme/models/mail_activity.py
@@ -51,7 +51,8 @@ class MailActivity(models.Model):
     @api.multi
     def unlink(self):
         user = self.env['res.users'].browse(self._uid)
-        if not self.env.context.get('safe_unlink', False) and not user.has_group('base.group_system'):
-            _logger.info("SAFE UNLINK {} - {}".format(self.mapped('res_name'),self.mapped('user_id.name')))
-            raise ValidationError("You are not authorized to cancel this activity.")
+        for act in self:
+            if not self.env.context.get('safe_unlink', False) and not user.has_group('base.group_system'):
+                _logger.info("SAFE UNLINK {} - {}".format(act.res_name,act.user_id.name))
+                raise ValidationError("You are not authorized to cancel this activity.")
         return super(MailActivity, self).unlink()
