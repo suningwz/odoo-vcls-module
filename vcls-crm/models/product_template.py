f64fcc902ec59afe980a16f7032adc801ee369e5
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-12-09 16:34:49 +0100

    Update product_template.py

--- vcls-crm/models/product_template.py
+++ vcls-crm/models/product_template.py
@@ -57,7 +57,7 @@ class ProductTemplate(models.Model):
     @api.depends('seniority_level_id','type','recurring_invoice','sale_ok','purchase_ok','can_be_expensed','service_policy','service_tracking')
     def _get_vcls_type(self):
         for product in self:
-            _logger.info("VCLS TYPE {}".format(product.name))
+            
             if product.type == 'service' and product.sale_ok and not product.purchase_ok and not product.can_be_expensed and not product.recurring_invoice and product.service_policy=='delivered_manual':
                 product.vcls_type = 'vcls_service'
             if product.seniority_level_id:
@@ -66,6 +66,8 @@ class ProductTemplate(models.Model):
                 product.vcls_type = 'subscription'   
             else:
                 product.vcls_type = 'other'
+                
+            _logger.info("VCLS TYPE {} - {}, {}, {}, {}, {}, {}".format(product.vcls_type,product.name, product.sale_ok,product.purchase_ok, product.can_be_expensed, product.recurring_invoice, product.service_policy))
 
 
 class Product(models.Model):
