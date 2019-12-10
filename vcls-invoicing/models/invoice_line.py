ffc88fa6248c7dc55864dd57b6134ec0e832a2dc
Azer ghadhoun <ghahdoun@voisinconsulting.com>
2019-12-10 17:41:10 +0100

    [IMPL] #838991 invoice_layout_next_step

--- vcls-invoicing/models/invoice_line.py
+++ vcls-invoicing/models/invoice_line.py
@@ -10,6 +10,25 @@ _logger = logging.getLogger(__name__)
 class InvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
 
+    section_line_id = fields.Many2one(
+        'account.invoice.line',
+        string='Line section',
+        compute='_get_section_line_id',
+        store=False
+    )
+
+    @api.multi
+    def _get_section_line_id(self):
+        for line in self:
+            invoice_line_ids = line.invoice_id.invoice_line_ids
+            current_section_line_id = False
+            for invoice_line_id in invoice_line_ids:
+                if invoice_line_id.display_type == 'line_section':
+                    current_section_line_id = invoice_line_id
+                elif line == invoice_line_id:
+                    line.section_line_id = current_section_line_id
+                    break
+
     @api.model
     def create(self, values):
         result = super(InvoiceLine, self).create(values)
