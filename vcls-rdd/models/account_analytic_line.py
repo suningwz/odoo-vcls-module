e06ae4a843c5da35379951bdfa8b01ccc57c84ab
mehdi <M9wtETqb>
2019-12-11 11:04:07 +0100

    [RDD] :update vcls-rdd module

---
+++ vcls-rdd/models/account_analytic_line.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+# (C) 2019 Smile (<http://www.smile.fr>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models, api
+
+from server.odoo.exceptions import UserError
+
+
+class AccountAnalyticLine(models.Model):
+    _inherit = 'account.analytic.line'
+
+    old_id = fields.Char(copy=False, readonly=True)
+
+    @api.model
+    def _update_project_soline_mapping(self, vals):
+        employee = None
+        if 'employee_id' in vals:
+            employee = self.env['hr.employee'].browse(vals['employee_id'])
+        elif self.env.user.employee_ids:
+            employee = self.env.user.employee_ids[0]
+        if employee and 'project_id' in vals:
+            project = self.env['project.project'].browse(vals['project_id'])
+            so = project.sale_order_id
+            list_default_rate = employee.default_rate_ids
+            if all(not sol.product_id.seniority_level_id
+                   for sol in so.order_line):
+                # the sale order does not exist, or it does not use seniority
+                # products at all -> don't enforce seniority on employee
+                # use case : projects not related to a SO or totally fixed
+                # price projects
+                return
+            if not employee.seniority_level_id and not self.env.user.context_data_integration:
+                raise UserError(
+                    _('''Employee with no seniority level can not timesheet
+                            on project, contact your HR department.
+                         '''
+                      )
+                )
+            # Check for existing mapping in project configuration
+            employee_mapped_line = project.sale_line_employee_ids.filtered(
+                lambda r: r.employee_id.id == employee.id
+            )
+            if employee_mapped_line:
+                # already mapped -> nothing to do
+                return
+            so_mapped_seniority = so.order_line.filtered(
+                lambda r: r.product_id.seniority_level_id != False
+            )
+            # Find a line on the default rate list
+            for so_line in so_mapped_seniority:
+                so_product = so_line.product_id
+                for default_rate in list_default_rate:
+                    if so_product.product_tmpl_id.id == default_rate.id:
+                        break
+            else:
+                # no line found, Find a line on the sale order with the same seniority level
+                for so_line in so.order_line:
+                    line_seniority = so_line.product_id.seniority_level_id
+                    if line_seniority == employee.seniority_level_id:
+                        break
+                else:
+                    # no line found, create the missing mapped line
+                    so_line = so.add_product_with_specific_seniority_level(
+                        employee)
+
+            project.sudo().write(
+                {
+                    'sale_line_employee_ids': [
+                        (
+                            0,
+                            False,
+                            {
+                                'employee_id': employee.id,
+                                'sale_line_id': so_line.id,
+                            },
+                        )
+                    ]
+                }
+            )
--- vcls-rdd/models/sale_order.py
+++ vcls-rdd/models/sale_order.py
@@ -29,15 +29,110 @@ class SaleOrder(models.Model):
             return super(SaleOrder, self).get_alpha_index(index)
         return 'SF'
 
+    @api.multi
+    def create_invoice_from_analytic_line(self):
+        for order in self:
+            if not self.env['account.invoice'].search([('origin', '=', order.name)]):
+                parent_project, child_projects = order._get_family_projects()
+                family_projects = (parent_project | child_projects)
+                line_ids = self.env['account.analytic.line'].search(
+                    [('stage_id', '=', 'invoiced'), ('old_id', '!=', False),
+                     ('project_id', 'in', family_projects.ids)])
+                if line_ids:
+                    invoice_vals = {'partner_id': order.partner_id.id,
+                                    'origin': order.name,
+                                    'po_id': order.po_id.id,
+                                    'type': 'out_invoice',
+                                    'account_id': order.partner_invoice_id.property_account_receivable_id.id,
+                                    'state': 'paid'}
+                    ail_vals = []
+                    for line in line_ids:
+                        task = line.task_id
+                        vals = (0, 0, {
+                            'product_id': task.sale_line_id.product_id.id,
+                            'price_unit': line.so_line_unit_price,
+                            'account_analytic_id': task.sale_line_id.order_id.analytic_account_id.id,
+                            'quantity': line.unit_amount,
+                            'account_id': task.sale_line_id.order_id.partner_id.property_account_receivable_id.id,
+                            'name': task.sale_line_id.product_id.name,
+                            'sale_line_ids': [(6, 0, [task.sale_line_id.id])]})
+                        ail_vals.append(vals)
+                    invoice_vals['invoice_line_ids'] = ail_vals
+                    invoice_id = self.env['account.invoice'].sudo().create(invoice_vals)
+                    order.invoice_ids = [invoice_id.id]
+        return True
+
+    @api.multi
+    def delete_empty_sections(self):
+        for order in self:
+            datas = {self.env['sale.order.line']: self.env['sale.order.line']}
+            last_parent = self.env['sale.order.line']
+            for line in order.order_line:
+                if line.display_type != 'line_section':
+                    datas[last_parent] += line
+                else:
+                    datas.update({line: self.env['sale.order.line']})
+                    last_parent = line
+            for key,vals in datas.items():
+                if not vals and (key.name == 'Hourly Rates' or key.name == 'Subscriptions'):
+                    self._cr.execute("DELETE FROM sale_order_line where id in %s", (tuple(key.ids),))
+        return True
+
+    @api.multi
+    def delete_duplicate_hourly_rate_lines(self):
+        for order in self:
+            datas = {self.env['sale.order.line']: self.env['sale.order.line']}
+            last_parent = self.env['sale.order.line']
+            must_be_deleted = self.env['sale.order.line']
+            for line in order.order_line:
+                if line.display_type != 'line_section':
+                    datas[last_parent] += line
+                else:
+                    datas.update({line: self.env['sale.order.line']})
+                    last_parent = line
+            grouped_data = {}
+            named = {}
+            for key, vals in datas.items():
+                if vals and key.name == 'Hourly Rates':
+                    for val in vals:
+                        if val.name in grouped_data:
+                            if grouped_data[val.name] < val.price_unit:
+                                grouped_data[val.name] = val.price_unit
+                                must_be_deleted += named[val.name]
+                            else:
+                                must_be_deleted += val
+                        else:
+                            grouped_data[val.name] = val.price_unit
+                            named[val.name] = val
+            if must_be_deleted:
+                if len(must_be_deleted) > 1:
+                    self._cr.execute("DELETE FROM sale_order_line where id in {}".format(tuple(must_be_deleted.ids)))
+                else:
+                    self._cr.execute("DELETE FROM sale_order_line where id = {}".format(must_be_deleted.id))
+        return True
+
+    @api.multi
+    def _create_section(self, section):
+        self.ensure_one()
+        if section not in self.order_line.filtered(lambda l: l.display_type == 'line_section').mapped('name'):
+            self.env['sale.order.line'].create({
+                'display_type': 'line_section',
+                'name': section,
+                'order_id': self.id
+            })
+
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     old_id = fields.Char("Old Id", copy=False, readonly=True)
     milestone_date = fields.Date("Milestone date")
+    mig_qty_invoiced = fields.Float(readonly=True)
+    mig_qty_delivered = fields.Float(readonly=True)
+    section_name = fields.Char()  # For migration only to remove after
 
-    @api.model
-    def create(self, vals):
+    @api.multi
+    def _get_ts_invoicing_mode(self, vals):
         if self.env.user.context_data_integration and vals.get('ts_invoicing_mode') \
                 and not vals.get('display_type'):
             invoicing_mode = vals.get('ts_invoicing_mode')
@@ -46,8 +141,61 @@ class SaleOrderLine(models.Model):
                 order.ts_invoicing_mode = invoicing_mode
             elif order.ts_invoicing_mode != invoicing_mode:
                 if order.child_ids:
+                    if vals.get('section_name'):
+                        order.child_ids[0]._create_section(vals.get('section_name'))
+                        del vals['section_name']
                     vals['order_id'] = order.child_ids[0].id
                 else:
-                    new_order = order.copy({'ts_invoicing_mode': invoicing_mode, 'parent_id': order.id, 'order_line': []})
+                    new_order = order.copy({'ts_invoicing_mode': invoicing_mode,
+                                            'parent_id': order.id,
+                                            'order_line': []})
+                    if vals.get('section_name'):
+                        new_order._create_section(vals.get('section_name'))
+                        del vals['section_name']
                     vals['order_id'] = new_order.id
+        return vals
+
+    @api.model
+    def create(self, vals):
+        vals = self._get_ts_invoicing_mode(vals)
         return super(SaleOrderLine, self).create(vals)
+
+    @api.multi
+    def write(self, vals):
+        vals = self._get_ts_invoicing_mode(vals)
+        return super(SaleOrderLine, self).write(vals)
+
+    @api.depends('invoice_lines.invoice_id.state', 'invoice_lines.quantity')
+    def _get_invoice_qty(self):
+        """
+        Update qty_invoiced field with migrating value if mig_qty_invoiced is set
+        """
+        for line in self:
+            qty_invoiced = line.mig_qty_invoiced
+            if not qty_invoiced:
+                for invoice_line in line.invoice_lines:
+                    if invoice_line.invoice_id.state != 'cancel':
+                        if invoice_line.invoice_id.type == 'out_invoice':
+                            qty_invoiced += invoice_line.uom_id._compute_quantity(invoice_line.quantity, line.product_uom)
+                        elif invoice_line.invoice_id.type == 'out_refund':
+                            qty_invoiced -= invoice_line.uom_id._compute_quantity(invoice_line.quantity, line.product_uom)
+            line.qty_invoiced = qty_invoiced
+
+    @api.multi
+    @api.depends('qty_delivered_method', 'qty_delivered_manual', 'analytic_line_ids.so_line', 'analytic_line_ids.unit_amount', 'analytic_line_ids.product_uom_id')
+    def _compute_qty_delivered(self):
+        """
+        Update qty delivered with migrating value if mig_qty_delivered is set
+        """
+        lines_by_analytic = self.filtered(lambda sol: sol.qty_delivered_method == 'analytic')
+        mapping = lines_by_analytic._get_delivered_quantity_by_analytic([('amount', '<=', 0.0)])
+        for so_line in lines_by_analytic:
+            so_line.qty_delivered = mapping.get(so_line.id, 0.0)
+        # compute for manual lines
+        for line in self:
+            qty_delivered = line.mig_qty_delivered
+            if not qty_delivered and line.qty_delivered_method == 'manual':
+                qty_delivered = line.qty_delivered_manual or 0.0
+            line.qty_delivered = qty_delivered
+
+
