d9f748abaf970b1d92062140012ff2fbde9d2f6b
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-12-17 13:55:47 +0100

    Mobility Boulogne in expense

--- vcls-expenses/models/product.py
+++ vcls-expenses/models/product.py
@@ -19,14 +19,23 @@ class Product(models.Model):
     @api.model
     def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
         vcls_company = self._context.get('vcls_expense_company')
+        vcls_project = self._context.get('vcls_project')
 
+        #VCLS Mobility case
+        if vcls_project:
+            project = self.env['project.project'].browse(vcls_project)
+            if project.name == 'Mobility Boulogne':
+                _logger.info("MOBILITY {}".format(project.name))
+                product_ids = super(Product, self)._search(args, offset, None, order, count=count, access_rights_uid=access_rights_uid)
+                product_ids.filtered(lambda p: p.categ_id.name == 'Mobility Expenses')
+                return product_ids
+        
         #if we are in the context of a vcls custom search
         if vcls_company:
             _logger.info("COMPANY {}".format(vcls_company))
             product_ids = super(Product, self)._search(args, offset, None, order, count=count, access_rights_uid=access_rights_uid)
             products = self.browse(product_ids)
-            products = products.filtered(lambda p: (not p.company_id) or (p.company_id.id==vcls_company))
-            
+            products = products.filtered(lambda p: ((not p.company_id) or (p.company_id.id==vcls_company)) and (p.categ_id.name != 'Mobility Expenses'))
             return products.ids
 
         else:
