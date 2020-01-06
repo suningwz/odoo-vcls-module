8939407e09130360e50a6c9b3b53196219334f12
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-01-06 14:43:23 +0100

    compute attachment count trigger

--- vcls-invoicing/models/invoice.py
+++ vcls-invoicing/models/invoice.py
@@ -616,7 +616,9 @@ class Invoice(models.Model):
         if not report_template:
             raise ValidationError(_(message))
         # create attachment
-        return self._create_activity_attachment(report_template, report_name)
+        attachemnt =  self._create_activity_attachment(report_template, report_name)
+        self._compute_attachment_count()
+        return attachment
 
     @api.multi
     def action_generate_draft_invoice(self):
