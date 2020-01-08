624df355fb1d5b58821bbee88d52edab9d58d979
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-01-08 09:06:08 +0100

    Update contact.py

--- vcls-contact/models/contact.py
+++ vcls-contact/models/contact.py
@@ -3,6 +3,10 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
 
+import logging
+
+_logger = logging.getLogger(__name__)
+
 
 class CountryGroup(models.Model):
     _inherit = 'res.country.group'
