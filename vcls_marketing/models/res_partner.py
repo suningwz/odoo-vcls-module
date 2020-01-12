673335c28285ac94621bd2070cd8b92470ebf1cd
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-01-12 22:13:21 +0100

    Renaming

--- vcls_marketing/models/res_partner.py
+++ vcls_marketing/models/res_partner.py
@@ -9,11 +9,11 @@ class Contacts(models.Model):
 
     marketing_project_id = fields.Many2one(
         comodel_name = 'project.marketing',
-        string = "Campaign Type",
+        string = "Lead Source",
     )
 
     marketing_task_id = fields.Many2one(
         comodel_name = 'task.marketing',
-        string = "Campaign",
+        string = "Related Campaign",
     )
 
