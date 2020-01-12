07b0cc26b671498d1faf42c8c96055b87fffc392
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-01-12 22:02:17 +0100

    add fields to res.partner

--- vcls_marketing/models/__init__.py
+++ vcls_marketing/models/__init__.py
@@ -3,3 +3,4 @@
 from . import project_marketing
 from . import task_marketing
 from . import crm_lead
+from . import res_partner
---
+++ vcls_marketing/models/res_partner.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, http, _
+
+
+class Contacts(models.Model):
+
+    _inherit = 'res.partner'
+
+    marketing_project_id = fields.Many2one(
+        comodel_name = 'project.marketing',
+        string = "Campaign Type",
+    )
+
+    marketing_task_id = fields.Many2one(
+        comodel_name = 'task.marketing',
+        string = "Campaign",
+    )
+
