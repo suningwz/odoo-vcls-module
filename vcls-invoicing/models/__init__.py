9bcc7ab1effb814283b94f6b233b2b15e9c9a710
Azer ghadhoun <ghahdoun@voisinconsulting.com>
2020-01-15 17:36:58 +0100

    [IMPL] #847559 Invoice template

--- vcls-invoicing/models/__init__.py
+++ vcls-invoicing/models/__init__.py
@@ -3,6 +3,7 @@ from . import sale
 from . import contact
 from . import invoicingPo
 from . import invoice
-#from . import res_partner
 from . import product
 from . import invoice_line
+from . import res_bank
+from . import fiscal_position
