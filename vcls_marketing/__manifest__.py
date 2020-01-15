19aa9bcca4e5d0187c85b55ba687a2f38e8e1913
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-01-15 20:16:43 +0100

    Marketing rework

--- vcls_marketing/__manifest__.py
+++ vcls_marketing/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Vcls Marketing",
+    'name': "VCLS Marketing",
 
     'summary': """
         VCLS customs marketing module.""",
@@ -9,16 +9,17 @@
     'author': "VCLS",
     'website': "http://www.voisinconsulting.com",
     'category': 'Uncategorized',
-    'version': '0.0.4',
+    'version': '0.0.5',
     'depends': [
         'project',
         'contacts',
         'vcls-project',
         'vcls-crm',
         'vcls_security',
+        'vcls-expenses',
     ],
     'data': [
-        'security/ir.model.access.csv',
-        'views/marketing_views.xml',
+        #'security/ir.model.access.csv',
+        #'views/marketing_views.xml',
     ],
 }
