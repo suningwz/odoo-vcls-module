e71d49bb60f1290a07be8386da554d9fd5ddc093
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-01-16 00:17:09 +0100

    default project values

--- vcls_marketing/models/project_marketing.py
+++ vcls_marketing/models/project_marketing.py
@@ -18,4 +18,20 @@ class Project(models.Model):
         string = 'Project Type',
     )
 
+    ###############
+    # ORM METHODS #
+    ###############
+    @api.model
+    def create(self, vals):
+
+        project = super(Project, self).create(vals)
+
+        if project.project_type == 'marketing':
+            project.company_id = self.env.ref('base.main_company')
+            wt = self.env['resource.calendar'].search([('company_id','=',project.company_id.id),('effective_hours','=',40.0)],limit=1)
+            if wt:
+                project.resource_calendar_id = wt
+            else:
+                project.resource_calendar_id = False
+
 
