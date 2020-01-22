fd40df4d284feda69ade4f96ad718d95d04609e7
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-01-22 17:45:09 +0100

    Extend lead partner data

--- vcls_marketing/models/crm_lead.py
+++ vcls_marketing/models/crm_lead.py
@@ -63,4 +63,36 @@ class Leads(models.Model):
             lead.marketing_task_out_id = lead.partner_id.marketing_task_out_id
             lead.opted_in_date = lead.partner_id.opted_in_date
             lead.opted_out_date = lead.partner_id.opted_out_date
+    
+    @api.multi
+    def _create_lead_partner_data(self, name, is_company, parent_id=False):
+        """ extract data from lead to create a partner
+            :param name : furtur name of the partner
+            :param is_company : True if the partner is a company
+            :param parent_id : id of the parent partner (False if no parent)
+            :returns res.partner record
+        """
+        data = super()._create_lead_partner_data(name, is_company, parent_id)
+        data['marketing_project_id'] = self.marketing_project_id.id
+        data['marketing_task_id'] = self.marketing_task_id.id
+        
+        if is_company:
+            pass
+        else:
+            data['marketing_task_out_id'] = self.marketing_task_out_id.id
+            #data['opted_in_date'] = self.opted_in_date
+            #data['opted_out_date'] = self.opted_out_date
+
+        return data
+    
+    def all_campaigns_pop_up(self):
+        model_id = self.env['ir.model'].search([('model','=','crm.lead')], limit = 1)
+        return {
+            'name': 'All participated events',
+            'view_mode': 'tree',
+            'target': 'new',
+            'res_model': 'marketing.participant',
+            'type': 'ir.actions.act_window',
+            'domain': "[('model_id','=', {}),('res_id','=',{})]".format(model_id.id, self.id)
+        }
 
