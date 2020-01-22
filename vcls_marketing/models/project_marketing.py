949925c0bbbf57b56b7d27d6fb090dd14d112146
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-01-22 16:23:06 +0100

    Update project_marketing.py

--- vcls_marketing/models/project_marketing.py
+++ vcls_marketing/models/project_marketing.py
@@ -40,16 +40,16 @@ class Project(models.Model):
         
         return project
 
-    """@api.multi
+    @api.multi
     def tasks_tree_view(self):
         action = self.env.ref('project.act_project_project_2_project_task_all').read()[0]
-        _logger.info("TASK: {}".format(action))
+        #_logger.info("TASK: {}".format(action))
         for project in self:
             if project.project_type == 'marketing':
                 action = self.env.ref('vcls_marketing.act_marketing_project_2_marketing_task_all').read()[0]
-                _logger.info("TASKM: {}".format(action))
+                #_logger.info("TASKM: {}".format(action))
             else:
                 pass
-        return action"""
+        return action
 
 
