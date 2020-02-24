a51f63ad68e9be88b276b7e820f9d7ddddd63b5f
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-02-24 21:49:09 +0100

    Update mapOdoo.py

--- vcls-etl/models/mapOdoo.py
+++ vcls-etl/models/mapOdoo.py
@@ -19,7 +19,34 @@ class mapOdoo(models.Model):
     
     @api.model
     def convertRef(self,SF,odoo,model,forMany): # convertRef
-        element = []
+        results = []
+        to_find = SF.split(';')
+        for item in to_find:
+            found = self.search([('externalName','=ilike',item),('odModelName','=',model)],limit=1)
+            if found: #a map exist
+                results.append(int(found.odooId))
+            else: #we create a new map
+                new_map = self.env[model].search([('name','ilike',item)],limit=1)
+                if new_map:
+                    results.append(new_map.id)
+                    self.create({
+                        'adModelName':model,
+                        'externalName':item.lower(),
+                        'odooId':str(new_map.id),
+                        'stage':1,
+                    })
+                else:
+                    self.create({
+                        'adModelName':model,
+                        'externalName':item.lower(),
+                        'stage':1,
+                    })
+                    
+        return results
+
+
+
+        """element = []
         SF = SF.split(';')
         for sfname in SF:
             try:
@@ -53,4 +80,4 @@ class mapOdoo(models.Model):
             return result
 
         #There is no mapping object for this SFName    
-        raise ITranslator.KeyNotFoundError
+        raise ITranslator.KeyNotFoundError"""
