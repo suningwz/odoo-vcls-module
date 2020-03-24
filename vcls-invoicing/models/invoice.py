606b347cb70a4e6623950ee6798b05703550c26e
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-03-24 21:17:24 +0100

    fix context for creation of com rate line

--- vcls-invoicing/models/invoice.py
+++ vcls-invoicing/models/invoice.py
@@ -598,7 +598,7 @@ class Invoice(models.Model):
                         name: line_cache[name]
                         for name in line_cache._cache
                     })
-                    invoice_line_obj.with_context(create_communication=True)\
+                    invoice_line_obj.with_context(create_communication=True,force_company=inv.company_id.id)\
                         .create(line_values)
                     #_logger.info("COM RATE LINE {}".format(line_values))
         return ret
