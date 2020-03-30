d962bd58e8c791c4ccdc704455a355776b38c330
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-03-30 08:05:52 +0200

    Reporting Task Id for follow-up

--- vcls-timesheet/__manifest__.py
+++ vcls-timesheet/__manifest__.py
@@ -16,7 +16,7 @@
     # for the full list
     'category': 'Uncategorized',
 
-    'version': '0.3.27',
+    'version': '0.3.28',
 
 
     # any module necessary for this one to work correctly
--- vcls-timesheet/models/analytic_account.py
+++ vcls-timesheet/models/analytic_account.py
@@ -38,6 +38,11 @@ class AnalyticLine(models.Model):
         store=True,
     )
 
+    reporting_task_id = fields.Many2one(
+        comodel_name = 'project.task',
+        compute = '_compute_reporting_task',
+    )
+
     # Used in order to group by client
     partner_id = fields.Many2one(
         'res.partner',
@@ -102,6 +107,11 @@ class AnalyticLine(models.Model):
         store = True,
         default = 'na',
         )
+    
+    @api.depends('task_id','task_id.parent_id')
+    def _compute_reporting_task(self):
+        for ts in self:
+            ts.reporting_task_id = ts.task_id.parent_id if ts.task_id.parent_id else ts.task_id
 
     @api.depends('project_id')
     def _compute_billability(self):
