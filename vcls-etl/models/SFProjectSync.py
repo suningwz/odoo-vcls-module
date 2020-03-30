b86419cc1f31653ce6fb6a47e58e62ccd46f91c3
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-03-30 22:11:31 +0200

    Update SFProjectSync.py

--- vcls-etl/models/SFProjectSync.py
+++ vcls-etl/models/SFProjectSync.py
@@ -329,8 +329,8 @@ class SFProjectSync(models.Model):
             #we check if this employee is already mapped in the project
             if employee not in project_id.sale_line_employee_ids.mapped('employee_id'):
                 product = self.sf_id_to_odoo_rec(assignment['KimbleOne__ActivityRole__c'])
-                _logger.info("EMPLOYEE MAP | Employee {} not mapped | Product from role {}".format(employee.name,product.name))
-                rate_lines = so_line.order_id.order_line.filtered(lambda l: l.name == product.name)
+                _logger.info("EMPLOYEE MAP | Employee {} not mapped | Product from role {} id {}".format(employee.name,product.name,product.id))
+                rate_lines = so_line.order_id.order_line.filtered(lambda l: l.name == product.name.split(' (copy)')[0])
                 _logger.info("EMPLOYEE MAP Rate Lines Found {}".format(rate_lines.mapped('name')))
                 map_vals = {
                     'employee_id': employee.id,
