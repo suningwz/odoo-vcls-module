ba44d246a5e3f6ec4693e826beade9097c7bf659
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-03-31 12:53:00 +0200

    Update res_bank.py

--- vcls-invoicing/models/res_bank.py
+++ vcls-invoicing/models/res_bank.py
@@ -16,6 +16,11 @@ class ResPartnerBank(models.Model):
     def name_get(self):
         result = []
         for account in self:
-            tmp_name = "{} | {}".format(account.journal_id.name, account.acc_number)
+            if account.journal_id and account.acc_number:
+                tmp_name = "{} | {}".format(account.journal_id.mapped('name'), account.acc_number)
+            else:
+                tmp_name = account.acc_number
+            
             result.append((account.id, tmp_name))
-            return result
+
+        return result
