2f031da142ed9d1338b86617adb85f21980645aa
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-04-05 20:29:01 +0200

    Simplify vcls-rdd

--- vcls-rdd/models/__init__.py
+++ vcls-rdd/models/__init__.py
@@ -5,5 +5,5 @@ from . import agreement
 from . import project
 from . import invoicing_po
 from . import lead
-from . import account_analytic_line
+#from . import account_analytic_line
 from . import azure_ad_change_queuer
