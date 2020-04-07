2e0ebb3bc81256d9cae241783e18d2962d93e78e
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-04-07 15:39:51 +0200

    Test invoice action cancel

--- vcls-invoicing/models/invoice.py
+++ vcls-invoicing/models/invoice.py
@@ -626,7 +626,28 @@ class Invoice(models.Model):
             ret = super(Invoice, invoice).unlink()
             
         orders.mapped('order_line')._compute_qty_delivered()
-        return ret
+        return 
+    
+    @api.multi
+    def action_cancel(self):
+        _logger.info("INVOICE ACTION CANCEL")
+        """moves = self.env['account.move']
+        for inv in self:
+            if inv.move_id:
+                moves += inv.move_id
+            #unreconcile all journal items of the invoice, since the cancellation will unlink them anyway
+            inv.move_id.line_ids.filtered(lambda x: x.account_id.reconcile).remove_move_reconcile()
+
+        # First, set the invoices as cancelled and detach the move ids
+        self.write({'state': 'cancel', 'move_id': False})
+        if moves:
+            # second, invalidate the move(s)
+            moves.button_cancel()
+            # delete the move this invoice was pointing to
+            # Note that the corresponding move_lines and move_reconciles
+            # will be automatically deleted too
+            moves.unlink()"""
+        return super(Invoice, self).action_cancel()
 
     @api.multi
     def html_to_string(self, html_format):
