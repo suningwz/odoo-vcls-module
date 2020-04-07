f7a3fc30dae3a51334fb018c373292d05a945ab0
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-04-07 07:34:43 +0200

    VC.006338

--- vcls-hr/__manifest__.py
+++ vcls-hr/__manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '1.15.4',
+    'version': '1.15.5',
 
 
     # any module necessary for this one to work correctly
