add9862c4563d7c4a9bbbdd7a34016e6d8576b5b
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-04-13 21:55:56 +0200

    Account Move values in EUR

--- vcls-accounting/models/account_move_line.py
+++ vcls-accounting/models/account_move_line.py
@@ -17,3 +17,38 @@ class AccountAnalyticLine(models.Model):
     account_code = fields.Char(
         related = 'account_id.code'
     )
+
+    base_currency_id = fields.Many2one('res.currency',compute="_compute_base_values", store=True)
+    debit_base_currency = fields.Monetary(default=0.0, currency_field='base_currency_id',compute="_compute_base_values",store=True)
+    credit_base_currency = fields.Monetary(default=0.0, currency_field='base_currency_id',compute="_compute_base_values",store=True)
+    convertion_rate = fields.Float(compute="_compute_base_values",store=True)
+
+    @api.depends('debit','credit','company_currency_id')
+    def _compute_base_values(self):
+        for line in self.filtered(lambda l: l.debit>0 and l.company_currency_id):
+            currency_id = self.env.ref('base.EUR')
+            debit_conv = line.company_currency_id._convert(
+                line.debit,
+                currency_id,
+                self.env.user.company_id,
+                line.date or fields.Datetime.now(),
+            )
+            line.write({
+                'base_currency_id':currency_id.id,
+                'convertion_rate':line.company_currency_id.rate,
+                'debit_base_currency':debit_conv,
+            })
+
+        for line in self.filtered(lambda l: l.credit>0 and l.company_currency_id):
+            currency_id = self.env.ref('base.EUR')
+            credit_conv = line.company_currency_id._convert(
+                line.credit,
+                currency_id,
+                self.env.user.company_id,
+                line.date or fields.Datetime.now(),
+            )
+            line.write({
+                'base_currency_id':currency_id.id,
+                'convertion_rate':line.company_currency_id.rate,
+                'credit_base_currency':credit_conv,
+            })
