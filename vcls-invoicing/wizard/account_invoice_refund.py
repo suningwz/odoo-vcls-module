0d2f74b67863721c30c0998c7efff54cb0e16f9d
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-04-22 22:18:13 +0200

    Final Credit Note implementation

--- vcls-invoicing/wizard/account_invoice_refund.py
+++ vcls-invoicing/wizard/account_invoice_refund.py
@@ -18,25 +18,23 @@ class AccountInvoiceRefund(models.TransientModel):
         ('cancel', 'Cancel: create credit note and reconcile'), 
         #('modify', 'Modify: create credit note, reconcile and create a new draft invoice') #we comment the modify case
         ],
-        default='refund', string='Credit Method', required=True, help='Choose how you want to credit this invoice. You cannot Modify and Cancel if the invoice is already reconciled')
+        default='cancel', string='Credit Method', required=True, help='Choose how you want to credit this invoice. You cannot Modify and Cancel if the invoice is already reconciled')
 
     @api.multi
     def compute_refund(self, mode='refund'):
         to_process = self.env['account.invoice'].browse(self._context.get('active_ids',False))
-        if to_process:
-            _logger.info("INVOICES TO REFUND {}".format(to_process.mapped('id')))
         ret = super().compute_refund(mode)
 
         if mode in ('cancel', 'modify'):
             #if cancel or modify, we release the potential timesheets and add the invoice ref to the so_line for a proper invoiced_qty calculation
             sale_lines = self.env['sale.order.line']
             for inv in to_process:
-                _logger.info("INVOICE TO REFUND {}".format(inv.id))
+                _logger.info("INVOICE TO REFUND {}".format(inv.number))
                 inv.release_timesheets()
                 rinv = self.env['account.invoice'].search([('type','=','out_refund'),('origin','=',inv.number)],limit=1)
                 if rinv:
                     _logger.info("Found Credit note {} for invoice {}".format(rinv.number,inv.number))
-                    for inv_line in inv.invoice_line_ids.filtered(lambda l: l.sale_line_ids):
+                    for inv_line in inv.invoice_line_ids.filtered(lambda l: l.sale_line_ids and l.product_id):
                         rinv_line = rinv.invoice_line_ids.filtered(lambda r: r.product_id == inv_line.product_id)
                         if rinv_line:
                             _logger.info("Found Matching Line {} {} {} {}".format(inv_line.product_id.name,inv_line.quantity,rinv_line.product_id.name,rinv_line.quantity))
@@ -45,13 +43,6 @@ class AccountInvoiceRefund(models.TransientModel):
                                 'invoice_lines':[(4, rinv_line.id, 0)],
                             })
                             sale_lines |= inv_line.sale_line_ids
-
-                """for inv_line in inv.invoice_line_ids:
-                    _logger.info("Found Invoice Line {}".format(inv_line.name))
-                    inv_line.sale_line_ids.write({
-                        'invoice_lines':[(4, inv_line.id, 0)],
-                    })
-                    sale_lines |= inv_line.sale_line_ids"""
             
             sale_lines._get_invoice_qty()
 
