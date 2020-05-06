f837fd90677b136343d11398d18299cc590dae2e
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2020-05-06 23:04:11 +0200

    [ADD] vcls_subscription

---
+++ vcls_subscription/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "vcls_subscription",
+
+    'summary': """
+        VCLS customs for subscriptions.""",
+
+    'description': """
+    """,
+
+    'author': "VCLS",
+    'website': "http://www.voisinconsulting.com",
+    'category': 'Uncategorized',
+    'version': '0.0.3',
+    'depends': [
+        'base',
+        'sale_subscription',
+    ],
+
+    'data': [
+        ### VIEWS ###
+        'views/sale_subscription_views.xml',
+        ### SECURITY ###
+        
+    ],
+
+    'demo': [
+    ],
+}
